4ed3d8a77701297302dd7fa54b960393fb2d1b60
Janderson <janderson2@mtech.edu>
2018-02-21 01:05:52 -0700

    Was continuing on making components, actions aren't playing nice.

--- webApp/app/routes/admin/courses.js
+++ webApp/app/routes/admin/courses.js
@@ -1,4 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	model() {
+		return 	{
+			program: [
+			{
+				semester: [
+				{
+					course:	[
+					{
+						courseName: 'MathyStuffy',
+						sectionNum: '01',
+						courseID:'32345'
+					}],
+					semesterName: 'Fall 2017',
+					semesterID: '32323'
+				}],
+				programName: 'Computer Sciences',
+				programID: '3232'
+			}]
+		};
+	}
+
 });
