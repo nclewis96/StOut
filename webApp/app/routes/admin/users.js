b00cb7bc5504a68857c6ffa635db3d9b1d4ddd9e
Trevor Brooks <tbrooks@mtech.edu>
2018-03-07 22:58:54 -0700

    Added a way to grab the user data from the backend response from routes. See admin->users

--- webApp/app/routes/admin/users.js
+++ webApp/app/routes/admin/users.js
@@ -1,19 +1,21 @@
 import Ember from 'ember';
 
 import CasAuthenticatedRouteMixin from '../../mixins/cas-authenticated-route';
+const { inject: { service } } = Ember;
 export default Ember.Route.extend(CasAuthenticatedRouteMixin, {
-	model() {
-		return 	{
-			programs: [
-			{
-				users: [
-				{
-					name: 'Jesse Anderson',
-					id: '3232'
-				}],
-			name: 'Computer Science',
-			id: '32382'
-			}]
-		};
-	}
+  currentUser: service(),
+  model() {
+    return 	{
+      programs: [
+        {
+          users: [
+            {
+              name: this.get('currentUser.name'),
+              id: this.get('currentUser.id') 
+            }],
+          name: 'Computer Science',
+          id: '32382'
+        }]
+    };
+  }
 });
