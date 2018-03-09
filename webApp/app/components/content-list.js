e9c5aa91c1730163636bc99f35a3413ffcd6993e
Jesse Anderson <Janderson2@mtech.edu>
2018-03-09 15:52:02 -0700

    Added actions, dynamic component, current route.

--- webApp/app/components/content-list.js
+++ webApp/app/components/content-list.js
@@ -1,14 +1,40 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-	classNames: ['primary'],
-	counter: 0,
+	routing: Ember.inject.service('-routing'),
+	subRoute: Ember.computed(function() {
+		return this.get('routing.currentRouteName').split(".")[1]
+	}),
+    renderType: null,
+	controlType: null,
+    permission: Ember.computed(function() {
+		return this.get('routing.currentRouteName').split(".")[0]
+	}),
+    data: null,
+    renderActions: null,
+	renderedComponent: Ember.computed('renderType', 'controlType', function() {
+        return `${this.get('renderType')}-${this.get('controlType')}`;
+    }),
+	
 	actions:  {
-		listSelected(object) {
-			this.incrementProperty('counter');
-			console.log("hi");
-			console.log(object.name);
-			console.log(object.id);
-		}
+        onItemClicked(data) {
+            this.setProperties({
+                data: data,
+                renderType: 'display',
+				controlType: 'control'
+            });
+        },
+        onEditClicked() {
+            this.setProperties({
+				renderType: 'form',
+				controlType: 'edit'
+			});
+        },
+        onRenderButtonClick(renderType, controlType) {
+            this.setProperties({
+				renderType: renderType,
+				controlType: controlType
+			});
+        }
 	}
 });
