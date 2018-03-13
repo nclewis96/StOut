573daeb47eb044a8a3a393ddc8faf7fc3779451e
Janderson <janderson2@mtech.edu>
2018-03-13 03:28:43 -0600

    Add tagNames to most, classNames to most, idNames to some.

--- webApp/app/components/content-list.js
+++ webApp/app/components/content-list.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	tagName: 'div',
+	idName: 'content-container',
 	routing: Ember.inject.service('-routing'),
 	subRoute: Ember.computed(function() {
 		return this.get('routing.currentRouteName').split(".")[1]
