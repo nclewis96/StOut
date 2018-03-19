acf6a6cbb7ff19c0d11317a4f3ce9014cb2d0c2a
Janderson <janderson2@mtech.edu>
2018-03-18 21:50:12 -0600

    Created display-components that buttions(links) to forms or components can be inserted into

--- webApp/tests/integration/components/display-admin-test.js
+++ webApp/tests/integration/components/display-admin-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('display-users', 'Integration | Component | display users', {
+moduleForComponent('display-admin', 'Integration | Component | display admin', {
   integration: true
 });
 
@@ -10,15 +10,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{display-users}}`);
+  this.render(hbs`{{display-admin}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#display-users}}
+    {{#display-admin}}
       template block text
-    {{/display-users}}
+    {{/display-admin}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
--- webApp/tests/integration/components/display-program-coordinator-test.js
+++ webApp/tests/integration/components/display-program-coordinator-test.js
@@ -1,7 +1,7 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
 
-moduleForComponent('display-courses', 'Integration | Component | display courses', {
+moduleForComponent('display-program-coordinator', 'Integration | Component | display program coordinator', {
   integration: true
 });
 
@@ -10,15 +10,15 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{display-courses}}`);
+  this.render(hbs`{{display-program-coordinator}}`);
 
   assert.equal(this.$().text().trim(), '');
 
   // Template block usage:
   this.render(hbs`
-    {{#display-courses}}
+    {{#display-program-coordinator}}
       template block text
-    {{/display-courses}}
+    {{/display-program-coordinator}}
   `);
 
   assert.equal(this.$().text().trim(), 'template block text');
