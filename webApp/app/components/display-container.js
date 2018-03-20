4fa8473984f2ddc6b1b0ecccfb0b6827b4f36fb4
Janderson <janderson2@mtech.edu>
2018-03-19 18:12:51 -0600

    Added css tags/classes, work is changing needs new branch, ready to merge.

--- webApp/app/components/display-container.js
+++ webApp/app/components/display-container.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	tagName: 'div',
+	classNames: ['container'],
+	
 	renderedComponent: Ember.computed('permission', function() {
         return `display-${this.get('permission')}`;
     }),
--- webApp/app/components/form-container.js
+++ webApp/app/components/form-container.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	tagName: 'div',
+	classNames: ['container'],
 	renderedComponent: Ember.computed('controlType', function() {
         return `form-${this.get('controlType')}`;
     })
