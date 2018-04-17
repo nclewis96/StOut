620c91afb651ab5a56fda69727013bb6c62f8304
nlewis <nlewis@mtech.edu>
2018-04-17 15:34:35 -0600

    Program restrictions to Metric ResourceList

--- backend/stout/src/main/java/edu/mtech/stout/resources/MetricResourceList.java
+++ backend/stout/src/main/java/edu/mtech/stout/resources/MetricResourceList.java
@@ -13,6 +13,7 @@ import javax.annotation.security.RolesAllowed;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.List;
+import java.util.Optional;
 
 @Path("/metrics")
 @Produces(MediaType.APPLICATION_JSON)
@@ -30,8 +31,17 @@ public class MetricResourceList {
   @POST
   @RolesAllowed({"Admin", "Program Coordinator"})
   @UnitOfWork
-  public Metric createMetric(Metric metric) {
-    return dao.create(metric);
+  public Metric createMetric(@Auth User user, Metric metric) {
+    Optional<Metric> m = dao.findById(metric.getId());
+    if(m.isPresent()){
+      if(queryBySelector.queryByProgramId(user,m.get().getProgramId())){
+        return dao.create(metric);
+      }
+      throw new NotAuthorizedException("Cannot create Metric not in your program");
+    }else{
+      throw new NotFoundException("No metrics are available in your program.");
+    }
+
   }
 
   @GET
@@ -39,10 +49,17 @@ public class MetricResourceList {
   @UnitOfWork
   public List<Metric> getMetricList(@Auth User user, @QueryParam("programId") LongParam programId) {
     if (queryBySelector.queryByProgramId(user, programId)) {
-      return dao.findByProgramId(programId.get());
-    } else {
-      return dao.findAll();
+      if (queryBySelector.queryUserPermForProg(user, programId.get(), 2)) {
+        return dao.findByProgramId(programId.get());
+      } else {
+        return dao.findAll();
+      }
+    }else{
+      throw new NotAuthorizedException("Cannot retrieve Metrics not in your program");
     }
+
+
+
   }
 
 }
